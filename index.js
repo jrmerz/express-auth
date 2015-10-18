0feaf59317f2169427693bc55de58024e5ac48fb
Justin Merz <jrmerz@gmail.com>
2015-10-17 22:26:19 -0700

    nightly checkin

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
+
+
 var setup = require('./lib/setup');
 var authorization = require('./lib/models/authorization');
 var authentication = require('./lib/controllers/authentication');
 var admin = require('./lib/controllers/admin');
+var account = require('./lib/controllers/account');
 
 module.exports = function(authSetup) {
+
+  // you can turn this off via bodyParserInstalled flag
+  if( !authSetup.bodyParserInstalled ) {
+    var bodyParser = require('body-parser');
+    // parse application/x-www-form-urlencoded
+    authSetup.app.use(bodyParser.urlencoded({ extended: true }));
+    // parse application/json
+    authSetup.app.use(bodyParser.json());
+  }
+
   setup.init(authSetup);
 
   // init the authentication (passport)
@@ -14,4 +27,9 @@ module.exports = function(authSetup) {
 
   // init the admin functionality
   admin();
+
+  // setup account endpoints
+  account();
+
+  return setup;
 };
