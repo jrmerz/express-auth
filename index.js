61e3cba97506c2ea0c75c12fba99d52a204e1574
Justin Merz <jrmerz@gmail.com>
2015-11-20 16:42:53 -0800

    not requiring express app to be provided

--- index.js
+++ index.js
@@ -1,35 +1,40 @@
 
 
-var setup = require('./lib/setup');
+var authSetup = require('./lib/setup');
 var authorization = require('./lib/models/authorization');
 var authentication = require('./lib/controllers/authentication');
 var admin = require('./lib/controllers/admin');
 var account = require('./lib/controllers/account');
 
-module.exports = function(authSetup) {
+module.exports = {
+  init : function(config) {
 
-  // you can turn this off via bodyParserInstalled flag
-  if( !authSetup.bodyParserInstalled ) {
-    var bodyParser = require('body-parser');
-    // parse application/x-www-form-urlencoded
-    authSetup.app.use(bodyParser.urlencoded({ extended: true }));
-    // parse application/json
-    authSetup.app.use(bodyParser.json());
+    // you can turn this off via bodyParserInstalled flag
+    if( !config.bodyParserInstalled && config.app ) {
+      var bodyParser = require('body-parser');
+      // parse application/x-www-form-urlencoded
+      config.app.use(bodyParser.urlencoded({ extended: true }));
+      // parse application/json
+      config.app.use(bodyParser.json());
+    }
+
+    authSetup.init(config);
+
+    // init the authentication (passport)
+    authentication();
+
+    // init the authorization (acl)
+    authorization();
+
+    // init the admin functionality
+    admin();
+
+    // setup account endpoints
+    account();
+
+    return authSetup;
+  },
+  getSetup : function() {
+    return authSetup;
   }
-
-  setup.init(authSetup);
-
-  // init the authentication (passport)
-  authentication();
-
-  // init the authorization (acl)
-  authorization();
-
-  // init the admin functionality
-  admin();
-
-  // setup account endpoints
-  account();
-
-  return setup;
 };
